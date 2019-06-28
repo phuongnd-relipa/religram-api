5f8a6f82e4b2c2b9a6a11cd9092eb31d6a68b4a7
HoaiNV <hoainv@relipasoft.com>
2019-06-28 13:14:38 +0700

    Task #123 - Fix CORS

--- src/main/java/com/relipa/religram/configuration/SecurityConfig.java
+++ src/main/java/com/relipa/religram/configuration/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.relipa.religram.configuration.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,6 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //@formatter:off
         http
                 .httpBasic().disable()
+                .cors().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
