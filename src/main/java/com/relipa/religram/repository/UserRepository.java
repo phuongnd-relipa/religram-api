938a430e5b3799798d8b1600b5e97aae323906d3
HoaiNV <hoainv@relipasoft.com>
2019-08-07 10:48:33 +0700

    Task #28 - Validate Unique username

--- src/main/java/com/relipa/religram/repository/UserRepository.java
+++ src/main/java/com/relipa/religram/repository/UserRepository.java
@@ -16,4 +16,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    Long countByUsernameAndIdNot(String username, Long Id);
+
 }
