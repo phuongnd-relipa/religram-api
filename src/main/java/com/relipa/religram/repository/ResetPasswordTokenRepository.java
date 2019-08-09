994081e0f47591c398ac6c981207a25647803462
HoaiNV <hoainv@relipasoft.com>
2019-08-09 11:57:00 +0700

    Task #30 - Update trường cấp token email

--- src/main/java/com/relipa/religram/repository/ResetPasswordTokenRepository.java
+++ src/main/java/com/relipa/religram/repository/ResetPasswordTokenRepository.java
@@ -8,6 +8,10 @@ import com.relipa.religram.entity.ResetPasswordToken;
 import com.relipa.religram.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ResetPasswordTokenRepository extends CrudRepository<ResetPasswordToken, Long> {
+
+    Optional<ResetPasswordToken> findFirstByUser(User user);
 
 }
